256bdffc41f88ca85f63ab32fa5b7e31681d7d44
przemek milke <theblimek@gmail.com>
2024-03-07 15:00:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet2_7_1
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int a = 5;
+            int b = 5;
+
+            if (a == b)
+
+            {
+                Console.WriteLine("Zmienna a i b są równe");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Zmienna a i b wcale nie są równe");
+                Console.ReadLine();
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
